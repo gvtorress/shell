42ada19947e2c3dd0122fb5c4c5d49d82ab40927
Gabriel Torres <gvtorress@gmail.com>
2026-03-28 16:39:56 -0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -6,10 +6,13 @@ const rl = createInterface({
   prompt: "$ ",
 });
 
-// TODO: Uncomment the code below to pass the first stage
 rl.prompt();
 
 rl.on('line', (command) => {
+  if (command === 'exit') {
+    rl.close();
+    return;
+  }
   console.log(`${command}: command not found`);
   rl.prompt();
 });
