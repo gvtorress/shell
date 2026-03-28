72122a82230cf9af2969bfd12e4193986ad3f701
Gabriel Torres <gvtorress@gmail.com>
2026-03-28 17:59:47 -0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -12,7 +12,10 @@ rl.on('line', (command) => {
   if (command === 'exit') {
     rl.close();
     return;
+  } else if (command.split(' ')[0] === 'echo') {
+    console.log(command.split(' ').slice(1).join(' '));
+  } else {
+    console.log(`${command}: command not found`);
   }
-  console.log(`${command}: command not found`);
   rl.prompt();
 });
