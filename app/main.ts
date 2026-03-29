62dba590a0f375940c7b1075fec78b86e3997a63
Gabriel Torres <gvtorress@gmail.com>
2026-03-28 23:34:58 -0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,5 @@
+import path from "path";
+import * as fs from "fs/promises"
 import { createInterface } from "readline";
 
 const rl = createInterface({
@@ -6,28 +8,66 @@ const rl = createInterface({
   prompt: "$ ",
 });
 
-const builtin_functions = new Set(['exit', 'echo', 'type']);
+const builtinFunctions = new Set(['exit', 'echo', 'type']);
+const PATH = "/usr/bin:/usr/local/bin:$PATH"
 
 rl.prompt();
 
-rl.on('line', (input) => {
-  const arrCommand = input.split(' ');
+rl.on('line', async (input) => {
+  const arrCommand = input.trim().split(/\s+/);
   const command = arrCommand[0];
+
+  if (!builtinFunctions.has(command)) {
+    console.log(`${command}: command not found`);
+    rl.prompt();
+    return;
+  }
 
   if (command === 'exit') {
     rl.close();
     return;
-  } else if (command === 'echo') {
-    console.log(arrCommand.slice(1).join(' '));
-  } else if (command === 'type') {
-    const commandType = arrCommand[1];
-    if (builtin_functions.has(commandType)) {
-      console.log(`${commandType} is a shell builtin`);
-    } else {
-      console.log(`${commandType}: not found`);
-    }
-  } else {
-    console.log(`${command}: command not found`);
   }
+
+  if (command === 'echo') {
+    console.log(arrCommand.slice(1).join(' '));
+  }
+
+  if (command === 'type') {
+    if (arrCommand.length > 1) {
+      const commandType = arrCommand[1];
+  
+      if (builtinFunctions.has(commandType)) {
+        console.log(`${commandType} is a shell builtin`);
+      } else {
+        const commandPath = await findPath(commandType);
+        commandPath ? console.log(`${commandType} is ${commandPath}`) : console.log(`${commandType}: not found`);
+      }
+    }
+  }
+
   rl.prompt();
 });
+
+const findPath = async (command: string): Promise<string | undefined> => {
+  const delimiter = path.delimiter;
+  const paths = PATH.split(delimiter);
+
+  for (const commandPath of paths) {
+    const fullCommand = path.join(commandPath, command);
+
+    if (await hasAccessPermission(fullCommand)) return fullCommand;
+  }
+
+  return undefined;
+}
+
+const hasAccessPermission = async (filePath: string): Promise<boolean> => {
+  try {
+    const stats = await fs.stat(filePath);
+    if (!stats.isFile()) return false;
+    await fs.access(filePath, fs.constants.X_OK);
+    return true;
+  } catch {
+    return false;
+  }
+}
